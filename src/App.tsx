64b857185aa1ceda45f39bed6cf48ee25dbfe86c
Tony Wu <cs.tony.wu@gmail.com>
2023-08-24 14:31:06 -0400

    Initial Setup with Typescript, Tailwind setup with Vite

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+  const [galleryView, setGalleryView] = useState(false)
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
